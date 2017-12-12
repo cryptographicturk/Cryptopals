c262b7d9c078d78e970b771697ab60c34996d695
Adrian Glover <adrianglover@Adrians-MacBook-Pro.local>
2017-12-12 09:02:56 -0800

    Fixed the bugs after actually running. Realize now that my algorithm is totally wrong.

--- hextobase64.js
+++ hextobase64.js
@@ -1,4 +1,4 @@
-const hexToDigit = function(hexIn) {
+const hexDigitToDecimal = function(hexIn) {
         return '0123456789abcdefgh'.indexOf(hexIn);
 }
 
@@ -6,19 +6,21 @@ const digitToBase64 = function(digitIn) {
         return 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'[digitIn];
 }
 
-const hextobase64 = function(hexIn) {
-        base64 = "";
-        hexLength = hexIn.length();
+const hexToBase64 = function(hexIn) {
+        base64 = '';
+        hexLength = hexIn.length;
         iterator = 0;
-        innerIterator = 4;
+        innerIterator = 3;
+	value = 0;
         while(hexLength > iterator) {
-                value += Math.pow(16, innerIterator) * hexToDigit(hexIn[iterator]);
+                value += Math.pow(16, innerIterator) * hexDigitToDecimal(hexIn[iterator]);
                 innerIterator--;
-                if(innerIterator == 0) {
-                        base64.append(digitToBase64(value));
-                        innerIterator = 4;
+                if(innerIterator < 0) {
+                        base64 += digitToBase64(value);
+                        innerIterator = 3;
                         value = 0;
                 }
+		iterator++;
         }
         return base64;
 }
